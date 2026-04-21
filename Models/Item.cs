dd49b5298ffb9841fcae2f48b28c104661c7fc62
TakahashiNg <83152264+TakahashiNguyen@users.noreply.github.com>
2026-04-21 15:42:08 +0700

    chore: update

---
+++ Models/Item.cs
@@ -0,0 +1,14 @@
+﻿namespace TH_502045_2.Models
+{
+    public class Item
+    {
+        public int ItemID { get; set; }
+        public string ItemName { get; set; }
+        public string Size { get; set; }
+
+        // Thêm các thuộc tính khác nếu cần (...)
+
+        // Quan hệ 1-N với OrderDetail
+        public ICollection<OrderDetail> OrderDetails { get; set; }
+    }
+}
